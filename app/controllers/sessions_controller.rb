5077a30d194bec5de07ade3a326b6f7e746eb5d3
Thomas Hur <thur1@binghamton.edu>
2021-04-19 01:27:36 +0000

    part 1 finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,33 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def nested_hash nh, indent
+    puts indent + "VALUE is a nested hash"
+    indent += '  '
+    nh.each_pair do |key, value|
+      puts indent + "KEY: #{key}"
+      if value.kind_of?(Hash)
+        nested_hash(value, indent)
+      else
+        puts indent + "VALUE:  #{value}"
+      end     
+    end
+  end
+    
   def create
+    auth_hash = request.env['omniauth.auth']
+      
+    puts '\n raw auth_hash\n'
+    p auth_hash
+    puts '\n\nauth_hash by key\n'
+    auth_hash.each_pair do |key, value|
+      puts "\nKEY: #{key}"
+      if value.kind_of?(Hash)
+        nested_hash(value, '')
+      else
+        puts "Value: #{value}"
+      end
+    end
   end
 
   def failure
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,7 +5,41 @@
 # files.
 
 require 'cucumber/rails'
+require 'omniauth'
 
+Before('@omniauth_test1') do
+  OmniAuth.config.test_mode = true
+  Capybara.default_host = 'http://example.com'
+
+  OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+    :provider => 'github',
+    :uid => '12345678',
+    :info => {:name => 'Tester Suny', :email => 'tester@binghamton.edu' } })
+    
+  Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:github]  
+end
+
+After('@omniauth_test1') do
+  OmniAuth.config.test_mode = false
+  OmniAuth.config.mock_auth[:github] = nil
+end
+
+Before('@omniauth_test2') do
+  OmniAuth.config.test_mode = true
+  Capybara.default_host = 'http://example.com'
+
+  OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+    :provider => 'github',
+    :uid => '12345678',
+    :info => {:name => 'Tester Rando', :email => 'tester@rando.com' } })
+      
+  Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:github]  
+end
+
+After('@omniauth_test2') do
+  OmniAuth.config.test_mode = false
+  OmniAuth.config.mock_auth[:github] = nil
+end
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
