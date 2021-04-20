a7ec00163647bbb427ea02cd1a0ad6506cc763e3
Thomas Hur <thur1@binghamton.edu>
2021-04-20 21:26:25 +0000

    finished part 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ActiveRecord::Base
-  has_many :authorizations
+  has_many :authorizations, :dependent => :destroy
   validates :name, :email, :presence => true
+  validate :staff_or_student
+  def staff_or_student
+    errors.add(:email, "must have SUNY ID") if
+      email.split('@')[1] != "binghamton.edu"
+  end
   
+  
+  def self.create_with_omniauth info
+    create! do |user|
+      user.name = info["name"]
+      user.email = info["email"]
+    end
+  end
 end
